f2e8db0f26ca4a2034ac7f05ec808e0d7cee10f8
hschroedl <hansj.schroedl@gmail.com>
2014-07-31 16:03:55 +0200

    Add basic project
    Sample implementation for creating a gist is included.

---
+++ GistClient/Gist.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GistClient {
+    public class Gist {
+        public String description;
+
+        public string Public;
+
+        public string files;
+
+    }
+}
---
+++ GistClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RestSharp;
+using RestSharp.Deserializers;
+
+namespace GistClient {
+    class Program {
+        static void Main(string[] args) {
+            var client = new RestClient("https://api.github.com");
+            var request = new RestRequest("/gists", Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            var file = new JsonObject();
+            file.Add("myFile.txt", new { content = "this is some file content"});
+            var jsonObject = new JsonObject();
+            jsonObject.Add("description", "x2762-1s");
+            jsonObject.Add("files", file);
+            jsonObject.Add("public",true);
+            request.AddBody(jsonObject);
+            var response = client.Execute(request);
+            var deserializer = new JsonDeserializer();
+            var jsonResponse = deserializer.Deserialize<Dictionary<String,String>>(response);
+            Console.ReadLine();
+
+        }
+    }
+}
