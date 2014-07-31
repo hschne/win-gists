79dad3137127814227d4a2cf4574c615868a42d4
hschroedl <hansj.schroedl@gmail.com>
2014-07-31 21:39:46 +0200

    Add File Reader

---
+++ GistClient/FileReader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace GistClient
+{
+    public static class FileReader
+    {
+        public static String Read(String filepath){
+            using (var fileStream = new FileStream(filepath, FileMode.Open)){
+                using (var reader = new StreamReader(fileStream)){
+                    String content = reader.ReadToEnd();
+                    fileStream.Dispose();
+                    return content;
+                }
+            }
+        }
+
+        public static String GetFileName(String filepath){
+            return Path.GetFileName(filepath);
+        }
+
+        public static string GetFileDescription(String filepath){
+            return "File " + GetFileName(filepath) + ", uploaded by WinGists";
+        }
+    }
+}
